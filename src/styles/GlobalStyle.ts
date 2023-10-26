701e3c23be54e946730be592ad19bcd5e71ae29b
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-10-26 22:49:25 +0200

    Add GlobalStyles

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*, ::before, ::after {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  color: ${({ theme }) => theme.colors.main};
+  font-family: ${({ theme }) => theme.typography.family};
+}
+ ul,
+  li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  button,
+  input {
+    font-family: inherit;
+  }
+
+  h1,
+  h2 {
+    margin: 0;
+  }
+`;
+
+export default GlobalStyle;
