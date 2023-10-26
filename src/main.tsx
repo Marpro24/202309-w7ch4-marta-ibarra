05ec826e1ecfcbfdbda12c6dd6605ecd21901469
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-10-26 22:57:42 +0200

    Add import font styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import { ThemeProvider } from "styled-components";
+import "@fontsource/montserrat";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/400-italic.css";
+import GlobalStyle from "./styles/GlobalStyle";
+import mainTheme from "./styles/mainTheme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div />
-  </React.StrictMode>
+    <ThemeProvider theme={mainTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
